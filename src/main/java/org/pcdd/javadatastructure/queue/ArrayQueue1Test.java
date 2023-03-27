ac4c533d0f174d6c34e108ac40fd09d9d3cf7db4
pcdd <hello_pcdd@163.com>
2023-03-27 22:19:10 +0800

    :tada: 首次提交队列的实现代码

---
+++ src/main/java/org/pcdd/javadatastructure/queue/ArrayQueue1Test.java
@@ -0,0 +1,18 @@
+package org.pcdd.javadatastructure.queue;
+
+/**
+ * @author pcdd
+ */
+public class ArrayQueue1Test {
+    public static void main(String[] args) {
+        ArrayQueue1<Integer> queue = new ArrayQueue1<>(3);
+        queue.offer(3366);
+        System.out.println(queue.peek());
+        queue.poll();
+        System.out.println(queue.peek());
+        queue.offer(4399);
+        System.out.println(queue.peek());
+
+        System.out.println(queue.size());
+    }
+}
---
+++ src/main/java/org/pcdd/javadatastructure/queue/Queue.java
@@ -0,0 +1,32 @@
+package org.pcdd.javadatastructure.queue;
+
+/**
+ * @author pcdd
+ */
+public interface Queue<E> extends Iterable<E> {
+
+    /**
+     * 队尾插入元素
+     */
+    boolean offer(E value);
+
+    /**
+     * 队头弹出元素，并返回
+     */
+    E poll();
+
+    /**
+     * 返回队头元素
+     */
+    E peek();
+
+    /**
+     * 返回元素个数
+     */
+    int size();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+}
